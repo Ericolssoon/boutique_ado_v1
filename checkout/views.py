84017ca6a2a6e11fc72e635a64ad1f1343349ab0
Ericolssoon <eric_olsson@hotmail.se>
2022-05-26 13:43:10 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51L3dbkCLUwyusPg9jc0UpewzHlKO6xodIhH87uNEtTdAiQACfHPYKOAs1Km96jY8oM3TesNKLPDVK6UKR6IIpFjB00EzhEcGbB',
+        'client_secret': 'test client secret',
     }
 
 
